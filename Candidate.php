b447ccec4283cd755fc22586e5cdef184a27ba34
DIKSHA KHARAT <40461809+diksha02031996@users.noreply.github.com>
2020-05-28 08:06:53 +0530

    Add files via upload

---
+++ Candidate.php
@@ -0,0 +1,81 @@
+<head>
+<title>Job Portal</title>
+<style>
+body {color: #fff; background: #000000; font: 24px Helvetica, Ariel, Sans-serif; background-image: linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(bg.jpg);}
+
+#header { width: 1300px; min-height: 150; margin: 0 auto; }
+#navigation { width: 1000px; margin-left: 195px; margin-top: 10px; }
+#navigation ul {margin: 0; padding: 0; list-style-type: none; }
+#navigation ul li {display: inline; margin-right: 3px; }
+#navigation ul li a {background: #000000; text-decoration: none; padding: 10px 10px 6px 10px; text-transform: uppercase; border-left: 1px solid #e4bc3f; border-top: 1px solid #e4bc3f; }
+#navigation ul li .current_page_item a, #navigation ul li a:hover  { color: #3f312e; border-left: 1px solid #fff; border-top: 1px solid #fff; border-top: 1px solid #fff; border-right: 1px solid #fff; background: #fff; }
+#container { overflow: auto; width: 960px; margin: 0 auto; }
+#content { width: 960px; min-height: 400px; margin: 0 auto; background: #000000; }
+#contentleft { width: 960px; float: left; margin-right: 20px; }
+#sidebar { width: 280px; float: left; }
+#footer { width: 960px; margin: 0 auto; background: #808080; }
+a:link, a:visited {
+  background-color: #000000;
+  color: white;
+  padding: 37px 43px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  position: relative;
+}
+
+a:hover, a:active {
+  background-color: black;
+}
+
+</style>
+</head>
+
+<body>
+<div id="header">
+<h1 style="text-align:center">India's Largest Job Portal</h1>
+
+
+<br><br></div>
+<div id="navigation">
+<ul>
+<li><a href="home.html">Home</a></li>
+<li><a href="postjob.html">Post Job</a></li>
+<li><a href="Candidate.html">Apply Job</a></li>
+<li><a href="">Skills</a></li>
+<li><a href="">Career Advice</a></li>
+<li><a href="">Query</a></li>
+</ul>
+
+</div>
+
+<br>
+
+<br><br><div id="container">
+
+
+<form>
+<h1>Find a job</h1>
+<div class="form-box">
+<input class="Search-Field Skills" name="search" type="text"
+placeholder="skills,keywords..."/>
+
+<input class="Search-Field location" name="location" type="text"
+placeholder="location..."/>
+
+<button class="search-btn" type="button">Search</button>
+
+</div>
+</form>
+<div id="contentleft">
+
+</div><!--contentleft-->
+
+<div id="sidebar">
+
+</div><!--sidebar-->
+
+</div><!--container-->
+<div id="footer">
+</div><!--footer-->
+</body>
